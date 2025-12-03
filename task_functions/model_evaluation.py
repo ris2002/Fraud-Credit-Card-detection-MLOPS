24faf667bb22c212546760d575127ce8834e6665
Rishil Boddula <rishilboddula@gmail.com>
2025-12-03 00:05:52 +0000

    Initialize DVC

--- task_functions/model_evaluation.py
+++ task_functions/model_evaluation.py
@@ -1,3 +1,12 @@
 import pandas as pd
-def model_eval(df)->None:
-    pass
+import logging
+import mlflow
+from helper_functions.helper_evaluator import Evaluation_And_Prediction_Strategy
+def model_eval()->None:
+    try:
+        evaluation=Evaluation_And_Prediction_Strategy()
+        model_prediction=evaluation.model_prediction()
+        
+    except Exception as e:
+        logging.error(f'Error Caused while Evaluating and Predictinng the model:{e}')
+        raise e
